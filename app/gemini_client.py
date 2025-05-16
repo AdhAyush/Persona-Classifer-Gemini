aff529b0c4d0d4f81322d9a5b741fab14356b7ae
Ayush <me@example.com>
2025-05-16 21:41:30 +0545

    trigger

--- app/gemini_client.py
+++ app/gemini_client.py
@@ -4,7 +4,7 @@ from app.config import settings
 genai.configure(api_key=settings.GEMINI_API_KEY)
 
 def get_persona(text: str) -> str:
-    prompt = f"What is the Persona (Optimistic, Pessimistic, Feiendly, Formal, Sarcastic, Neutral) of this text: {text}    \n give only the category as a response. Must choose from the following categories: Optimistic, Pessimistic, Friendly, Formal, Sarcastic, Neutral"
+    prompt = f"What is the Persona (Optimistic, Pessimistic, Feiendly, Formal, Sarcastic, Neutral) of this text : {text}    \n give only the category as a response. Must choose from the following categories: Optimistic, Pessimistic, Friendly, Formal, Sarcastic, Neutral"
     model = genai.GenerativeModel("gemini-2.0-flash")
     response = model.generate_content(prompt)
     return response.text.strip().lower()
--- tests/test_persona.py
+++ tests/test_persona.py
@@ -9,7 +9,7 @@ from app.main import app  # make sure app is defined in app/main.py
 client = TestClient(app)
 
 def test_persona_response():
-    sample_text = "I love this product!"
+    sample_text = "I love this product"
     response = client.post("/api/analyze", json={"text": sample_text})
 
     assert response.status_code == 200
