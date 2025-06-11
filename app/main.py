fb3aedeb280cd7a9f237722286d19b535a97da5b
Ayush <me@example.com>
2025-06-11 23:43:26 +0545

    index.html added for browser inference

--- app/main.py
+++ app/main.py
@@ -1,5 +1,17 @@
 from fastapi import FastAPI
 from app.routes import router
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+
+
+# Allow all origins for development
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Use ["http://localhost:5500"] etc. in production
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(router, prefix="/api")
